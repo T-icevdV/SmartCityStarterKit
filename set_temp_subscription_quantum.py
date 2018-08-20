6989dc23b82944e2277c5efa052728070ba4dcba
T-icevdV <thijs.vandervelden@atos.net>
2018-08-20 16:48:44 +0200

    Grafana and Crate-db

---
+++ set_temp_subscription_quantum.py
@@ -0,0 +1,8 @@
+import requests
+
+for i in range(1,36):
+	url = "http://localhost:1026/v2/subscriptions/"
+	header = {"Content-Type": "application/json" }
+	payload = '{"description": "Test subscription","subject": {"entities": [{"id": "ein-aireas-'+str(i)+'","type": "AirQualityObserved"}]},"notification": {"http": {"url": "http://quantum-leap:8668/v2/notify"},"attrs": [],"metadata": ["dateCreated", "dateModified"]},"throttling": 5}'
+	r = requests.post(url, data = payload, headers = header)
+	print(i)
