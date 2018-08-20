4c8dbbe6a63e4654d33acd4b124183b926347171
T-icevdV <thijs.vandervelden@atos.net>
2018-08-20 16:50:55 +0200

    Added airbox

--- set_temp_subscription_quantum.py
+++ set_temp_subscription_quantum.py
@@ -1,6 +1,6 @@
 import requests
 
-for i in range(1,36):
+for i in range(1,37):
 	url = "http://localhost:1026/v2/subscriptions/"
 	header = {"Content-Type": "application/json" }
 	payload = '{"description": "Test subscription","subject": {"entities": [{"id": "ein-aireas-'+str(i)+'","type": "AirQualityObserved"}]},"notification": {"http": {"url": "http://quantum-leap:8668/v2/notify"},"attrs": [],"metadata": ["dateCreated", "dateModified"]},"throttling": 5}'
